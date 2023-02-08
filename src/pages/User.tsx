2f1586033f2710b385640a548f325e0605820c62
seoyeon-jung <jungluchia1204@gmail.com>
2023-02-08 21:17:43 +0900

    design: User 페이지 프로필 UI 구현 #51
    User Page UI 구현했습니다
    - 닉네임 구현
    - 닉네임 변경하기 버튼 UI만 구현

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,5 +1,16 @@
 const User = () => {
-  return <>User</>;
+  return (
+    <>
+      <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+        <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+          <div className="font-3xl m-5 text-black font-bold">007님!</div>
+          <button className="text-sm px-4 py-2 leading-none border rounded text-black border-black hover:border-transparent hover:bg-blue-200 mt-4 lg:mt-0">
+            닉네임 변경하기
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default User;
