6b19ed56783fd060e5842464a4371a18010341bf
hiheecho <110316147+hiheecho@users.noreply.github.com>
2023-02-08 19:49:38 +0900

    design : create LikeBtn ui and toggle menu #49 #50
    좋아요버튼 ui를 생성합니다.
    반응형 디자인 중 수정삭제버튼을 토글메뉴로 수정합니다.
    해시태그 나타내는 부분을 컴포넌트화 합니다.

--- src/components/course/CourseTitle.tsx
+++ src/components/course/CourseTitle.tsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdOutlineMoreVert } from "react-icons/md";
 
 const CourseTitle = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="w-11/12 md:w-3/4 m-auto">
+    <div className="w-full">
       <div className="flex justify-space">
-        <div className="flex w-3/4 gap-5 md:gap-10 items-center h-24 ">
-          <h3 className="w-1/6 sm:w-1/10">지역명</h3>
+        <div className="flex w-3/4 gap-5 items-center h-24 ">
+          <h3 className="w-1/6 sm:w-1/10 text-lg">지역명</h3>
           <h2 className="md:w-5/6 text-2xl md:text-3xl ">제목</h2>
         </div>
         <div className="flex gap-3 justify-end w-1/3 items-center ">
-          <MdOutlineMoreVert className="sm:hidden" size={24} />
+          <MdOutlineMoreVert
+            className="sm:hidden"
+            size={24}
+            onClick={() => setOpen(!open)}
+          />
           <button className="bg-gray-300 px-4 sm:px-5 py-1 rounded-xl hidden sm:flex justify-center">
             수정
           </button>
@@ -19,19 +24,16 @@ const CourseTitle = () => {
           </button>
         </div>
       </div>
-      <div className="absolute right-2 top-16 flex flex-col gap-y-1">
-        <button className="bg-gray-300 px-4 sm:px-5 py-1 rounded-xl sm:hidden">
-          수정
-        </button>
-        <button className="bg-gray-300 px-4 sm:px-5 py-1 rounded-xl sm:hidden">
-          삭제
-        </button>
-      </div>
-      <div className="flex gap-3">
-        <h3 className="bg-gray-300 px-4 sm:px-5 py-1 rounded-xl flex justify-center">
-          #해시태그
-        </h3>
-      </div>
+      {open === true ? (
+        <div className="absolute right-8 top-16 flex flex-col gap-y-1">
+          <button className="bg-gray-300 px-4 sm:px-5 py-1 rounded-xl sm:hidden">
+            수정
+          </button>
+          <button className="bg-gray-300 px-4 sm:px-5 py-1 rounded-xl sm:hidden">
+            삭제
+          </button>
+        </div>
+      ) : null}
     </div>
   );
 };
--- src/pages/Course.tsx
+++ src/pages/Course.tsx
@@ -1,10 +1,15 @@
+import CommentInput from "../components/course/CommentInput";
+import CourseHashTag from "../components/course/CourseHashTag";
 import CourseTitle from "../components/course/CourseTitle";
+import LikeBtn from "../components/course/LikeBtn";
 
 const Course = () => {
   return (
-    <>
+    <div className="w-11/12 md:w-3/4 m-auto">
       <CourseTitle />
-    </>
+      <CourseHashTag />
+      <LikeBtn />
+    </div>
   );
 };
 
