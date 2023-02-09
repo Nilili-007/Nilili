dbb8aab18a62198d8e53457b1beee04ac987e235
seoyeon-jung <jungluchia1204@gmail.com>
2023-02-09 20:49:54 +0900

    refactor: header, footer 컴포넌트 위치 변경
    - useNavigate() 오류로 인해 App.tsx가 아닌 Router.tsx 파일 안으로 header와 footer 컴포넌트를 옮겼습니다
    - 모든 컴포넌트에서 navigate 기능 사용 가능합니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import Router from "./shared/Router";
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <>
-      <Header />
       <Router />
-      <Footer />
     </>
   );
 }
--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Main, Post, User, Course } from "../pages";
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/Post" element={<Post />} />
         <Route path="/user/:id" element={<User />} />
         <Route path="/:id" element={<Course />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
