31bd1bfc2e3b48f6f90f945c0ea98721d8f68f78
hiheecho <110316147+hiheecho@users.noreply.github.com>
2023-02-17 10:57:08 +0900

    feat : edit comment sort feature
    댓글 데이터 컴포넌트를 정렬별로 사용하지 않고, 하나의 컴포넌트 안에서 하나의 쿼리로만 동작하도록 수정합니다.

--- src/pages/Course.tsx
+++ src/pages/Course.tsx
@@ -5,14 +5,12 @@ import {
   CourseHashTag,
   CourseTitle,
   LikeBtn,
-  CommentAsc,
   CommentDesc,
 } from "../components/course";
 import { PostTitle, PostHashTag } from "../components/post";
 
 const Course = () => {
   const [isEdit, setIsEdit] = useState(false);
-  const [desc, setDesc] = useState(true);
   const paramId = useParams().id;
 
   return (
@@ -31,45 +29,9 @@ const Course = () => {
       ) : (
         <CourseHashTag />
       )}
-      <LikeBtn />
+      <LikeBtn paramId={paramId} />
       <CommentInput paramId={paramId} />
-      <div className="mb-10">
-        <div>
-          <input
-            id="desc"
-            type="button"
-            onClick={() => {
-              setDesc(true);
-            }}
-            value="최신순"
-            style={
-              desc === true
-                ? { fontWeight: 600, textDecoration: "underline" }
-                : undefined
-            }
-            className="mr-2 mb-4"
-          />
-          <input
-            id="asc"
-            type="button"
-            onClick={() => {
-              setDesc(false);
-            }}
-            value="오래된 순"
-            style={
-              desc === false
-                ? { fontWeight: 600, textDecoration: "underline" }
-                : undefined
-            }
-          />
-        </div>
-
-        {desc === true ? (
-          <CommentDesc paramId={paramId} />
-        ) : (
-          <CommentAsc paramId={paramId} />
-        )}
-      </div>
+      <CommentDesc paramId={paramId} />
     </div>
   );
 };
