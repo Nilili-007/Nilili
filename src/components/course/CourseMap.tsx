1316642c0a5e8836f76ffff7d4992f63d1b75559
cinephi1e <aprilkb412@naver.com>
2023-02-21 18:21:45 +0900

    feat: 커버 이미지, 제목, 닉네임, 작성 날짜 및 시간, 지도, 코스 #10

---
+++ src/components/course/CourseMap.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useRef, useState } from "react";
+import { Map, MapTypeControl } from "react-kakao-maps-sdk";
+import { CourseInfo, CourseMapMarker } from "./index";
+
+interface CourseProps {
+  course: CourseType | undefined;
+}
+
+const CourseMap = ({ course }: any) => {
+  const [map, setMap] = useState();
+  const [filteredId, setFilteredId] = useState("");
+  const courseList = JSON.parse(course?.courseList);
+  let bounds: any;
+
+  courseList.map((item: any) => {
+    bounds = item.bounds;
+    bounds = Object.setPrototypeOf(bounds, kakao.maps.LatLngBounds.prototype);
+    if (map === undefined) {
+      setMap(bounds);
+    }
+  });
+
+  useEffect(() => {
+    if (map !== undefined) {
+      // @ts-ignore
+      //   map.panTo(bounds);
+    }
+  }, [filteredId]);
+
+  //   장소 id가 변할 때마다 map.panTo
+
+  return (
+    <div className="flex w-full h-[70vh] my-14">
+      <Map
+        center={{
+          lat: courseList[0].position.lat,
+          lng: courseList[0].position.lng,
+        }}
+        level={8}
+        // @ts-ignore
+        onCreate={setMap}
+        className="w-[65%] h-full z-0"
+      >
+        <MapTypeControl position={kakao.maps.ControlPosition.TOPRIGHT} />
+        <CourseMapMarker
+          courseList={courseList}
+          filteredId={filteredId}
+          setFilteredId={setFilteredId}
+        />
+      </Map>
+      <CourseInfo
+        courseList={courseList}
+        filteredId={filteredId}
+        setFilteredId={setFilteredId}
+      />
+    </div>
+  );
+};
+
+export default CourseMap;
