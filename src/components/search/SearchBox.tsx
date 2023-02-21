35df32fad24952270beea7b97765e0923fa756da
hobak12 <tjsgud2564@naver.com>
2023-02-21 15:45:27 +0900

    refactor: select options import 해서 코드 정리

--- src/components/search/SearchBox.tsx
+++ src/components/search/SearchBox.tsx
@@ -4,43 +4,8 @@ import SearchList from "./SearchList";
 import { useGetCourseQuery } from "../../redux/modules/apiSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { changeHashTagNum } from "../../redux/modules/searchSlice";
-
-const regionOptions = [
-  { value: "서울", label: "서울" },
-  { value: "경기/인천", label: "경기/인천" },
-  { value: "충청남도", label: "충청남도" },
-  { value: "충청북도", label: "충청북도" },
-  { value: "강원도", label: "강원도" },
-  { value: "경상북도", label: "경상북도" },
-  { value: "경상남도", label: "경상남도" },
-  { value: "전라북도", label: "전라북도" },
-  { value: "전라남도", label: "전라남도" },
-  { value: "제주도", label: "제주도" },
-  { value: "부산", label: "부산" },
-  { value: "대구", label: "대구" },
-  { value: "대전", label: "대전" },
-  { value: "광주", label: "광주" },
-  { value: "울산", label: "울산" },
-];
-
-const hashTagOptions = [
-  { value: "#가족", label: "#가족" },
-  { value: "#친구", label: "#친구" },
-  { value: "#연인", label: "#연인" },
-  { value: "#아이들과", label: "#아이들과" },
-  { value: "#반려동물", label: "#반려동물" },
-  { value: "#혼자", label: "#혼자" },
-  { value: "#단체", label: "#단체" },
-  { value: "#힐링", label: "#힐링" },
-  { value: "#데이트", label: "#데이트" },
-  { value: "#쇼핑", label: "#쇼핑" },
-  { value: "#맛집", label: "#맛집" },
-  { value: "#카페", label: "#카페" },
-  { value: "#문화생활", label: "#문화생활" },
-  { value: "#캠핑", label: "#캠핑" },
-  { value: "#섬", label: "#섬" },
-  { value: "#역사", label: "#역사" },
-];
+import { hashTagOptions } from "../post/PostHashTag";
+import { regionOptions } from "../post/PostTitle";
 
 const travelStatusOptions = [
   { value: false, label: "여행 전" },
