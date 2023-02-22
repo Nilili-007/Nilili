d05b9f4bbd255ff26af2803ef47f7abe2c27dcea
hiheecho <110316147+hiheecho@users.noreply.github.com>
2023-02-22 21:10:03 +0900

    design: edit Course page UI
    코스페이지 UI 디자인 반영하여 수정

--- src/components/course/CommentInput.tsx
+++ src/components/course/CommentInput.tsx
@@ -17,6 +17,7 @@ const CommentInput = ({ paramId }: CommentProps) => {
   const submitRef = useRef<HTMLButtonElement | any>();
   const [addComment] = useAddCommentMutation();
   const commentValue = comment.trim();
+  const userImg: any = authService.currentUser?.photoURL;
   useEffect(() => {
     if (commentValue) {
       submitRef.current.disabled = false;
@@ -41,20 +42,32 @@ const CommentInput = ({ paramId }: CommentProps) => {
   return (
     <>
       <form className="mb-20" onSubmit={commentSubmitHandler}>
-        <textarea
-          wrap="hard"
-          cols={20}
-          className="border-2 resize-none px-2 py-1 w-full h-28"
-          placeholder={
-            authService.currentUser
-              ? "댓글을 입력해 주세요."
-              : "로그인 한 이용자만 이용하실 수 있습니다."
-          }
-          value={comment}
-          onChange={(event) => setComment(event.target.value)}
-        />
+        <div className="p-5">
+          <div className="flex items-middle gap-5 mb-5">
+            <img
+              src={userImg}
+              alt="profile Image"
+              className="object-fill w-[36px] h-[36px]"
+            />
+            <h3 className="text-[28px] font-bold">
+              {authService.currentUser?.displayName}
+            </h3>
+          </div>
+          <textarea
+            wrap="hard"
+            cols={20}
+            className="border-2 resize-none px-2 py-1 w-full h-28 text-[22px]"
+            placeholder={
+              authService.currentUser
+                ? "댓글을 입력해 주세요."
+                : "로그인 한 이용자만 이용하실 수 있습니다."
+            }
+            value={comment}
+            onChange={(event) => setComment(event.target.value)}
+          />
+        </div>
         <button
-          className="bg-blue-500 text-white font-bold py-2 px-4 rounded disabled:opacity-50"
+          className="w-[92px] h-[60px] bg-[#000000] text-white font-bold py-4 px-5 disabled:opacity-40 float-right"
           ref={submitRef}
           disabled
         >
