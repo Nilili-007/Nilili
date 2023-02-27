a7f7da02289ced02b86e84f460cf9e265c1c4265
seoyeon-jung <jungluchia1204@gmail.com>
2023-02-27 12:11:55 +0900

    feat: url 복사 기능 추가 #118
    - 버튼 클릭시 현재 url이 복사되는 기능을 추가했습니다

--- src/components/course/Share.tsx
+++ src/components/course/Share.tsx
@@ -4,6 +4,8 @@ import {
   TwitterIcon,
   TwitterShareButton,
 } from "react-share";
+// @ts-ignore
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 const Share = () => {
   // window 객체에서 현재 url 가져오기
@@ -14,12 +16,16 @@ const Share = () => {
       <h1>공유하기</h1>
       <div>
         <FacebookShareButton url={currentURL}>
-          <FacebookIcon size={30} round={true} borderRadius={24} />
+          <FacebookIcon size={40} round={true} borderRadius={24} />
         </FacebookShareButton>
         <TwitterShareButton url={currentURL}>
-          <TwitterIcon size={30} round={true} borderRadius={24} />
+          <TwitterIcon size={40} round={true} borderRadius={24} />
         </TwitterShareButton>
-        <button className="border border-red-500">url</button>
+        <CopyToClipboard text={currentURL}>
+          <button className="w-[40px] h-[40px] text-white borde-none rounded-full text-base bg-[#7362ff] hover:bg-[#a99fee]">
+            URL
+          </button>
+        </CopyToClipboard>
         <button className="border border-red-500">kakaotalk</button>
       </div>
     </div>
