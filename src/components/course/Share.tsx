cf1676f20b317b7d76732d88d3c2fc59ebbdb7f5
seoyeon-jung <jungluchia1204@gmail.com>
2023-02-27 11:41:56 +0900

    feat: facebook, twitter 공유 기능 추가 #118
    - react-share 라이브러리를 통해 facebook, twitter 공유 기능을 추가했습니다

--- src/components/course/Share.tsx
+++ src/components/course/Share.tsx
@@ -1,10 +1,27 @@
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  TwitterShareButton,
+} from "react-share";
+
 const Share = () => {
+  // window 객체에서 현재 url 가져오기
+  const currentURL = window.location.href;
+
   return (
     <div>
-      <button className="border border-red-500">facebook</button>
-      <button className="border border-red-500">twitter</button>
-      <button className="border border-red-500">url</button>
-      <button className="border border-red-500">kakaotalk</button>
+      <h1>공유하기</h1>
+      <div>
+        <FacebookShareButton url={currentURL}>
+          <FacebookIcon size={30} round={true} borderRadius={24} />
+        </FacebookShareButton>
+        <TwitterShareButton url={currentURL}>
+          <TwitterIcon size={30} round={true} borderRadius={24} />
+        </TwitterShareButton>
+        <button className="border border-red-500">url</button>
+        <button className="border border-red-500">kakaotalk</button>
+      </div>
     </div>
   );
 };
