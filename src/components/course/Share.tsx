2e2d7ee506035874e4a696a14ca631ed49d07241
seoyeon-jung <jungluchia1204@gmail.com>
2023-02-27 14:29:16 +0900

    feat: kakao 공유 기능 추가 #118
    - html에 script를 넣는 방법이 아닌 usehooks 사이트에서 만들어 놓은 custom hook인 useScript 파일을 생성했습니다
    - 카카오톡 공유 기능을 추가했습니다
    - 카카오 버튼 클릭 시 링크 공유가 가능합니다다

--- src/components/course/Share.tsx
+++ src/components/course/Share.tsx
@@ -6,10 +6,65 @@ import {
 } from "react-share";
 // @ts-ignore
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import { useScript } from "../../hooks/useScript";
+import { useEffect } from "react";
+
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
 
 const Share = () => {
   // window 객체에서 현재 url 가져오기
   const currentURL = window.location.href;
+
+  const handleKakaoBtn = () => {
+    window.Kakao.Share.sendDefault({
+      objectType: "feed",
+      content: {
+        title: "NILILI의 여행 코스를 공유합니다",
+        description: "NILILI의 여행 코스로 여행을 다녀오세요!",
+        imageUrl:
+          "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+        link: {
+          webUrl: currentURL,
+          mobileWebUrl: currentURL,
+        },
+      },
+      itemContent: {
+        profileText: "NILILI",
+        profileImageUrl:
+          "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+        titleImageUrl:
+          "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+      },
+      buttons: [
+        {
+          title: "자세히 보기",
+          link: {
+            webUrl: currentURL,
+            mobileWebUrl: currentURL,
+          },
+        },
+      ],
+    });
+  };
+
+  // kakao SDK import하기
+  const status = useScript("https://developers.kakao.com/sdk/js/kakao.js");
+
+  // kakao sdk 초기화하기
+  // status가 변경될 때마다 실행되며, status가 ready일 때 초기화를 시도합니다.
+  useEffect(() => {
+    if (status === "ready" && window.Kakao) {
+      // 중복 initialization 방지
+      if (!window.Kakao.isInitialized()) {
+        // 두번째 step 에서 가져온 javascript key 를 이용하여 initialize
+        window.Kakao.init(process.env.REACT_APP_KAKAO);
+      }
+    }
+  }, [status]);
 
   return (
     <div>
@@ -26,7 +81,12 @@ const Share = () => {
             URL
           </button>
         </CopyToClipboard>
-        <button className="border border-red-500">kakaotalk</button>
+        <button onClick={handleKakaoBtn}>
+          <img
+            src="https://developers.kakao.com/assets/img/about/logos/kakaolink/kakaolink_btn_medium.png"
+            alt="카카오링크 보내기 버튼"
+          />
+        </button>
       </div>
     </div>
   );
