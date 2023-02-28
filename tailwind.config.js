4d460b2c75087ceb2750cbfdaae60d25344a02a4
hobak12 <tjsgud2564@naver.com>
2023-02-28 22:52:13 +0900

    ui: global css 세팅 #142

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,29 @@ module.exports = {
     extend: {
       screens: {
         "3xl": "1700px",
+        sm: "414px",
         xs: { min: "375px", max: "414px" },
       },
+
+      colors: {
+        error: "#B3261E",
+        like: "#EC6762",
+        correct: "#50AA72",
+        gray: {
+          "00": "#FBFBFB",
+          "01": "#F7F8F9",
+          "02": "#E9EBED",
+          "03": "#CBCDD2",
+          "04": "#A0A4A8",
+          "05": "#474C51",
+          "06": "#262829",
+        },
+      },
+    },
+
+    fontFamily: {
+      sans: ["noto-sans-cjk-kr", "sans-serif"],
+      eng: ["Montserrat", "sans-serif"],
     },
   },
   variants: {
