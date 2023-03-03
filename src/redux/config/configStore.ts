b982a01104846b809cc3bfe8f027a2c8205e70c1
cinephi1e <aprilkb412@naver.com>
2023-03-03 19:57:59 +0900

    hotfix: 충돌 수정

--- src/redux/config/configStore.ts
+++ src/redux/config/configStore.ts
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import courseSlice from "../modules/courseSlice";
-import searchSlice from "../modules/searchSlice";
 import { courseApi } from "../modules/apiSlice";
 const store = configureStore({
   reducer: {
     [courseApi.reducerPath]: courseApi.reducer,
     courseSlice,
-    searchSlice,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }).concat(
