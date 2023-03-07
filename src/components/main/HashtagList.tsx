46067ec7d5fea0ad0d027608d3dd64b9a2002d47
hobak12 <tjsgud2564@naver.com>
2023-03-07 20:38:04 +0900

    ui: (메인) 해시태그 리스트 반응형 구현 #135

--- src/components/main/HashtagList.tsx
+++ src/components/main/HashtagList.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import { logEvent } from "../../utils/amplitude";
 import { hashTagOptions } from "../post/PostHashTag";
+import { useState } from "react";
+import { CgChevronDown, CgChevronUp } from "react-icons/cg";
 
 const HashtagList = () => {
   const hashTagAmplitudeEvent = () => {
@@ -8,22 +10,40 @@ const HashtagList = () => {
       from: "메인페이지 Hashtag",
     });
   };
+
+  const [openHash, setOpenHash] = useState(false);
+
   return (
-    <div className="mt-[10%] bg-gray-200 w-full h-[600px]">
-      <div className="mx-auto  lg:max-w-6xl w-[90%] min-h-[400px]   ">
+    <div
+      className={`mt-[10%] pb-[2%] bg-gray-200 w-full sm:max-h-[550px] overflow-hidden ${
+        openHash === false ? "max-h-[250px]" : "max-h-[500px]"
+      } `}
+    >
+      <div className="mx-auto  lg:max-w-6xl w-[90%] ">
         <p className="mt-[5%] mb-[2%]  ml-1 my-[2%] w-fit xl:text-[50px] lg:text-[45px] sm:text-[35px] text-3xl font-bold font-eng  ">
           HASHTAG IN KOREA
         </p>
         <p className=" hidden sm:block ml-2 pb-5 w-fit text-xl text-gray-04">
           NILILI 사용자가 가장 최근 올린 일정을 함께해보세요.
         </p>
-        <div className="flex flex-wrap  mt-[3%] font-hashtags w-[98%] ">
+        <div className="block sm:hidden">
+          {openHash === false ? (
+            <button onClick={() => setOpenHash(true)}>
+              <CgChevronDown className="text-3xl" />
+            </button>
+          ) : (
+            <button onClick={() => setOpenHash(false)}>
+              <CgChevronUp className="text-3xl" />
+            </button>
+          )}
+        </div>
+        <div className="flex flex-wrap  mt-[3%] font-hashtags w-[98%] overflow-x-auto whitespace-nowrap no-scrollbar ">
           <Link
             onClick={() => {
               hashTagAmplitudeEvent();
             }}
             to={`/search?ht=${JSON.stringify([hashTagOptions[0]])}`}
-            className=" border-b-[3px] w-[22%] pl-[0.3%] pt-2 mr-[3%] text-[25px] font-normal border-black mb-12 hover:bg-black hover:text-white "
+            className=" border-b-[3px] sm:w-[22%] w-[47%] pl-[0.3%] pt-2 mr-[3%] sm:text-[25px] text-base  font-normal border-black mb-[5%] hover:bg-black hover:text-white "
           >
             #화목하게 가족과
           </Link>
@@ -32,7 +52,7 @@ const HashtagList = () => {
               hashTagAmplitudeEvent();
             }}
             to={`/search?ht=${JSON.stringify([hashTagOptions[1]])}`}
-            className=" border-b-[3px]  w-[22%] pl-[0.3%] pt-2 mr-[3%] text-[25px] font-normal border-black mb-12 hover:bg-black hover:text-white "
+            className=" border-b-[3px] sm:w-[22%] w-[47%] pl-[0.3%] pt-2 mr-[3%] sm:text-[25px] text-base font-normal border-black mb-[5%] hover:bg-black hover:text-white "
           >
             #우정충전 친구와
           </Link>
@@ -41,7 +61,7 @@ const HashtagList = () => {
               hashTagAmplitudeEvent();
             }}
             to={`/search?ht=${JSON.stringify([hashTagOptions[2]])}`}
-            className=" border-b-[3px]  w-[22%] pl-[0.3%] pt-2 mr-[3%] text-[25px] font-normal border-black mb-12 hover:bg-black hover:text-white "
+            className=" border-b-[3px]  sm:w-[22%] w-[47%] pl-[0.3%] pt-2 mr-[3%] sm:text-[25px] text-base font-normal border-black mb-[5%] hover:bg-black hover:text-white "
           >
             #사랑하는 연인과
           </Link>
@@ -50,7 +70,7 @@ const HashtagList = () => {
               hashTagAmplitudeEvent();
             }}
             to={`/search?ht=${JSON.stringify([hashTagOptions[3]])}`}
-            className=" border-b-[3px]  w-[22%] pl-[0.3%] pt-2 mr-[3%] text-[25px] font-normal border-black mb-12 hover:bg-black hover:text-white "
+            className=" border-b-[3px] sm:w-[22%] w-[47%] pl-[0.3%] pt-2 mr-[3%] sm:text-[25px] text-base font-normal border-black mb-[5%] hover:bg-black hover:text-white "
           >
             #소중한 아이들과
           </Link>
@@ -59,7 +79,7 @@ const HashtagList = () => {
               hashTagAmplitudeEvent();
             }}
             to={`/search?ht=${JSON.stringify([hashTagOptions[4]])}`}
-            className=" border-b-[3px]  w-[22%] pl-[0.3%] pt-2 mr-[3%] text-[25px] font-normal border-black mb-12 hover:bg-black hover:text-white "
+            className=" border-b-[3px] sm:w-[22%] w-[47%] pl-[0.3%] pt-2 mr-[3%] sm:text-[25px] text-base font-normal border-black mb-[5%] hover:bg-black hover:text-white "
           >
             #반려동물과 같이
           </Link>
@@ -68,7 +88,7 @@ const HashtagList = () => {
               hashTagAmplitudeEvent();
             }}
             to={`/search?ht=${JSON.stringify([hashTagOptions[5]])}`}
-            className=" border-b-[3px]  w-[22%] pl-[0.3%] pt-2 mr-[3%] text-[25px] font-normal border-black mb-12 hover:bg-black hover:text-white "
+            className=" border-b-[3px]  sm:w-[22%] w-[47%] pl-[0.3%] pt-2 mr-[3%] sm:text-[25px] text-base font-normal border-black mb-[5%] hover:bg-black hover:text-white "
           >
             #조용히 나 혼자
           </Link>
@@ -77,7 +97,7 @@ const HashtagList = () => {
               hashTagAmplitudeEvent();
             }}
             to={`/search?ht=${JSON.stringify([hashTagOptions[6]])}`}
-            className=" border-b-[3px]  w-[22%] pl-[0.3%] pt-2 mr-[3%] text-[25px] font-normal border-black mb-12 hover:bg-black hover:text-white "
+            className=" border-b-[3px]  sm:w-[22%] w-[47%] pl-[0.3%] pt-2 mr-[3%] sm:text-[25px] text-base font-normal border-black mb-[5%] hover:bg-black hover:text-white "
           >
             #다 함께 단체로
           </Link>
@@ -86,7 +106,7 @@ const HashtagList = () => {
               hashTagAmplitudeEvent();
             }}
             to={`/search?ht=${JSON.stringify([hashTagOptions[7]])}`}
-            className=" border-b-[3px]  w-[22%] pl-[0.3%] pt-2 mr-[3%] text-[25px] font-normal border-black mb-12 hover:bg-black hover:text-white "
+            className=" border-b-[3px]  sm:w-[22%] w-[47%] pl-[0.3%] pt-2 mr-[3%] sm:text-[25px] text-base font-normal border-black mb-[5%] hover:bg-black hover:text-white "
           >
             #일상 속의 휴식
           </Link>
@@ -95,7 +115,7 @@ const HashtagList = () => {
               hashTagAmplitudeEvent();
             }}
             to={`/search?ht=${JSON.stringify([hashTagOptions[8]])}`}
-            className=" border-b-[3px]  w-[22%] pl-[0.3%] pt-2 mr-[3%] text-[25px] font-normal border-black mb-12 hover:bg-black hover:text-white "
+            className=" border-b-[3px]  sm:w-[22%] w-[47%] pl-[0.3%] pt-2 mr-[3%] sm:text-[25px] text-base font-normal border-black mb-[5%] hover:bg-black hover:text-white "
           >
             #로맨틱한 데이트
           </Link>
@@ -104,7 +124,7 @@ const HashtagList = () => {
               hashTagAmplitudeEvent();
             }}
             to={`/search?ht=${JSON.stringify([hashTagOptions[9]])}`}
-            className=" border-b-[3px]  w-[22%] pl-[0.3%] pt-2 mr-[3%] text-[25px] font-normal border-black mb-12 hover:bg-black hover:text-white "
+            className=" border-b-[3px]  sm:w-[22%] w-[47%] pl-[0.3%] pt-2 mr-[3%] sm:text-[25px] text-base font-normal border-black mb-[5%] hover:bg-black hover:text-white "
           >
             #FLEX! 쇼핑
           </Link>
@@ -113,7 +133,7 @@ const HashtagList = () => {
               hashTagAmplitudeEvent();
             }}
             to={`/search?ht=${JSON.stringify([hashTagOptions[10]])}`}
-            className=" border-b-[3px]  w-[22%] pl-[0.3%] pt-2 mr-[3%] text-[25px] font-normal border-black mb-12 hover:bg-black hover:text-white "
+            className=" border-b-[3px] sm:w-[22%] w-[47%] pl-[0.3%] pt-2 mr-[3%] sm:text-[25px] text-base font-normal border-black mb-[5%] hover:bg-black hover:text-white "
           >
             #나만 알고픈 맛집
           </Link>
@@ -122,7 +142,7 @@ const HashtagList = () => {
               hashTagAmplitudeEvent();
             }}
             to={`/search?ht=${JSON.stringify([hashTagOptions[11]])}`}
-            className=" border-b-[3px]  w-[22%] pl-[0.3%] pt-2 mr-[3%] text-[25px] font-normal border-black mb-12 hover:bg-black hover:text-white "
+            className=" border-b-[3px] sm:w-[22%] w-[47%] pl-[0.3%] pt-2 mr-[3%] sm:text-[25px] text-base font-normal border-black mb-[5%] hover:bg-black hover:text-white "
           >
             #감성 충천 카페
           </Link>
@@ -131,7 +151,7 @@ const HashtagList = () => {
               hashTagAmplitudeEvent();
             }}
             to={`/search?ht=${JSON.stringify([hashTagOptions[12]])}`}
-            className=" border-b-[3px]  w-[22%] pl-[0.3%] pt-2 mr-[3%] text-[25px] font-normal border-black mb-12 hover:bg-black hover:text-white "
+            className=" border-b-[3px]  sm:w-[22%] w-[47%] pl-[0.3%] pt-2 mr-[3%] sm:text-[25px] text-base font-normal border-black mb-[5%] hover:bg-black hover:text-white "
           >
             #교양충전 문화생활
           </Link>
@@ -140,7 +160,7 @@ const HashtagList = () => {
               hashTagAmplitudeEvent();
             }}
             to={`/search?ht=${JSON.stringify([hashTagOptions[13]])}`}
-            className=" border-b-[3px]  w-[22%] pl-[0.3%] pt-2 mr-[3%] text-[25px] font-normal border-black mb-12 hover:bg-black hover:text-white "
+            className=" border-b-[3px]  sm:w-[22%] w-[47%] pl-[0.3%] pt-2 mr-[3%] sm:text-[25px] text-base font-normal border-black mb-[5%] hover:bg-black hover:text-white "
           >
             #떠나봐요 캠핑
           </Link>
@@ -149,7 +169,7 @@ const HashtagList = () => {
               hashTagAmplitudeEvent();
             }}
             to={`/search?ht=${JSON.stringify([hashTagOptions[14]])}`}
-            className=" border-b-[3px] w-[22%] pl-[0.3%] pt-2 mr-[3%] text-[25px] font-normal border-black mb-12 hover:bg-black hover:text-white "
+            className=" border-b-[3px] sm:w-[22%] w-[47%] pl-[0.3%] pt-2 mr-[3%] sm:text-[25px] text-base font-normal border-black mb-[5%] hover:bg-black hover:text-white "
           >
             #배타고 섬으로
           </Link>
@@ -158,7 +178,7 @@ const HashtagList = () => {
               hashTagAmplitudeEvent();
             }}
             to={`/search?ht=${JSON.stringify([hashTagOptions[15]])}`}
-            className=" border-b-[3px] w-[22%] pl-[0.3%] pt-2 mr-[3%] text-[25px] font-normal border-black mb-12 hover:bg-black hover:text-white "
+            className=" border-b-[3px] sm:w-[22%] w-[47%] pl-[0.3%] pt-2 mr-[3%] sm:text-[25px] text-base font-normal border-black mb-[5%] hover:bg-black hover:text-white "
           >
             #깊은 역사 속으로
           </Link>
