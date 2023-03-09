86c8af08fb5316d74d861124e9200de598958deb
hiheecho <110316147+hiheecho@users.noreply.github.com>
2023-03-09 09:15:20 +0900

    fix : fix detail page bugs
    수정 시, 좋아요 수가 리셋되는 버그를 해결합니다.
    작성시간 일부가 1일로 나타나는 버그를 수정합니다.
    상세페이지 제목이 길어질 경우의 디자인을 추가합니다.

--- src/components/course/CourseHeader.tsx
+++ src/components/course/CourseHeader.tsx
@@ -12,16 +12,16 @@ const CourseHeader = ({ course }: CourseProps) => {
         />
       </div>
       <div className="w-full h-[220px] -mt-[220px] sm:h-[450px] sm:-mt-[450px] md:h-[700px] md:-mt-[700px] absolute z-10 bg-gradient-to-t from-[#00000060]" />
-      <div className="w-[85%] left-[15%] -mt-52 text-white absolute xs:left-[5%] xs:-mt-28">
+      <div className="w-[85%] sm:left-[5%] md:left-[15%] -mt-52 text-white absolute xs:left-[5%] xs:-mt-28">
         <div className=" w-full z-20 flex">
-          <h1 className="z-20 text-5xl font-bold leading-tight w-[70%] xs:text-xl xs:w-[90%]">
+          <h1 className="z-20 sm:text-2xl md:text-3xl xl:text-4xl 2xl:text-5xl font-bold leading-tight sm:w-[90%] lg:w-[80%] xs:text-xl xs:w-[90%]">
             {course?.title}
-            <span className="ml-1 z-20 text-xs sm:text-lg font-bold text-black bg-white opacity-80 px-2 py-1 sm:h-auto sm:top-0 xs:px-1.5 xs:py-0.5 ">
+            <span className="ml-1 z-20 text-xs md:text-[14px] 2xl:text-lg font-bold text-black bg-white opacity-80 px-2 py-1 sm:h-auto sm:top-0 xs:px-1.5 xs:py-0.5 ">
               {course?.travelStatus === true ? "여행 후" : "여행 전"}
             </span>
           </h1>
         </div>
-        <div className="mt-4 z-20 body3 sm:text-2xl flex xs:mt-2">
+        <div className="mt-4 z-20 body3 sm:text-xl lg:text-2xl flex xs:mt-2">
           {course?.location.map((location: any) => {
             return (
               <p className="pr-2 z-20" key={location}>
