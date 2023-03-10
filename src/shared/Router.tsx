c9f334feadaa265e896dec81ca718a67af01ecd4
hiheecho <110316147+hiheecho@users.noreply.github.com>
2023-03-10 22:41:02 +0900

    refactor : add lazy-loading
    코드 스플리팅을 적용합니다.

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,35 +1,38 @@
+import React, { Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
 import {
   Main,
-  Post,
-  User,
-  Course,
-  Search,
   NotFound,
-  EditCourse,
   Login,
   Register,
   AuthForgot,
+  Post,
+  User,
+  Course,
+  EditCourse,
+  Search,
 } from "../pages";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/post" element={<Post />} />
-        <Route path="/user/:id" element={<User />} />
-        <Route path="/course/:id" element={<Course />} />
-        <Route path="/edit/:id" element={<EditCourse />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/authforgot" element={<AuthForgot />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/post" element={<Post />} />
+          <Route path="/user/:id" element={<User />} />
+          <Route path="/course/:id" element={<Course />} />
+          <Route path="/edit/:id" element={<EditCourse />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/authforgot" element={<AuthForgot />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </BrowserRouter>
   );
