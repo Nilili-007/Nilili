f87dee75a502c310e79c81cd50cdb491bf9ced84
cinephi1e <aprilkb412@naver.com>
2023-03-13 10:42:02 +0900

    ui: 반응형 지도 마커 라인 위치 수정

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import { Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
