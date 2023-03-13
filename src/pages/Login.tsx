e3f77cbf6d179cf4c845e61960f20035e7c9267c
seoyeon-jung <jungluchia1204@gmail.com>
2023-03-13 09:41:13 +0900

    fix: height 지정
    - 로그인 페이지의 height를 지정했습니다
    - 회원가입 페이지의 height를 지정했습니다

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "../components/auth";
 
 const Login = () => {
   return (
-    <div className="flex flex-wrap flex-col items-center">
+    <div className="flex flex-wrap flex-col items-center h-[1100px]">
       <LoginForm />
     </div>
   );
